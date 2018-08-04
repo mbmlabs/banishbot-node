4b218a47011134dcc993c8ac78665c0b08e5a8ea
Mathew <mathew@mathewjenkinson.co.uk>
2018-08-04 11:43:03 -0700

    Updates to support PII data.

--- index.js
+++ index.js
@@ -12,20 +12,34 @@ config = {
 
 module.exports = {
   // Check BanishBot to see if an object has been banished or not.
-  checkBanishment: function(username, apiKey, banishItem, callback){
+  checkBanishment: function(username, apiKey, banishItem, pii, callback){
     var deferred = Q.defer();
-    // Assemble the URL Request
-    var url = config.baseURL+'/'+config.baseVersion+'/'+encodeURI(username)+'/'+encodeURI(banishItem)
     var method = 'GET';
 
-    var options = {
-      url: url,
-      method: method,
-      headers: {
-        'User-Agent': 'nodeSDK',
-        'x-api-key': apiKey
-      }
-    };
+    if(pii == true){
+      // Assemble the URL Request
+      var url = config.baseURL+'/'+config.baseVersion+'/'+encodeURI(username)+'/pii';
+      var options = {
+        url: url,
+        method: method,
+        headers: {
+          'User-Agent': 'nodeSDK',
+          'x-api-key': apiKey,
+          'x-pii': banishItem
+        }
+      };
+    } else {
+      // Assemble the URL Request
+      var url = config.baseURL+'/'+config.baseVersion+'/'+encodeURI(username)+'/'+encodeURI(banishItem);
+      var options = {
+        url: url,
+        method: method,
+        headers: {
+          'User-Agent': 'nodeSDK',
+          'x-api-key': apiKey
+        }
+      };
+    }
 
     function httpCallback(error, response, body) {
       if (!error && response.statusCode == 200) {
@@ -41,27 +55,41 @@ module.exports = {
   },
 
   // Banish a new Item to BanishBot, this may or maynot include a notes payload
-  banishNewItem: function(username, apiKey, banishItem, banishPayload, callback){
+  banishNewItem: function(username, apiKey, banishItem, banishPayload, pii, callback){
     var deferred = Q.defer();
-    // Assemble the URL Request
-    var url = config.baseURL+'/'+config.baseVersion+'/'+encodeURI(username)+'/'+encodeURI(banishItem)
     var method = 'POST';
+
     if(!banishPayload){
       banishPayload = {
         banished: true
       };
     }
 
-    var options = {
-      url: url,
-      method: method,
-      body: banishPayload,
-      json: true,
-      headers: {
-        'User-Agent': 'nodeSDK',
-        'x-api-key': apiKey
-      }
-    };
+    if(pii == true){
+      // Assemble the URL Request
+      var url = config.baseURL+'/'+config.baseVersion+'/'+encodeURI(username)+'/pii';
+      var options = {
+        url: url,
+        method: method,
+        headers: {
+          'User-Agent': 'nodeSDK',
+          'x-api-key': apiKey,
+          'x-pii': banishItem
+        }
+      };
+    } else {
+      // Assemble the URL Request
+      var url = config.baseURL+'/'+config.baseVersion+'/'+encodeURI(username)+'/'+encodeURI(banishItem);
+      var options = {
+        url: url,
+        method: method,
+        headers: {
+          'User-Agent': 'nodeSDK',
+          'x-api-key': apiKey
+        }
+      };
+    }
+
 
     function httpCallback(error, response, body) {
       if (!error && response.statusCode == 200) {
@@ -77,26 +105,39 @@ module.exports = {
   },
 
   // Update Banished Item, this may involve updating the banished state, user notes or type of banished object
-  updateBanishedItem: function(username, apiKey, banishItem, banishPayload, callback){
+  updateBanishedItem: function(username, apiKey, banishItem, banishPayload, pii, callback){
     var deferred = Q.defer();
-    // Assemble the URL Request
-    var url = config.baseURL+'/'+config.baseVersion+'/'+encodeURI(username)+'/'+encodeURI(banishItem)
     var method = 'PUT';
+
     if(!banishPayload['banished']){
       banishPayload = {
       };
     }
 
-    var options = {
-      url: url,
-      method: method,
-      body: banishPayload,
-      json: true,
-      headers: {
-        'User-Agent': 'nodeSDK',
-        'x-api-key': apiKey
-      }
-    };
+    if(pii == true){
+      // Assemble the URL Request
+      var url = config.baseURL+'/'+config.baseVersion+'/'+encodeURI(username)+'/pii';
+      var options = {
+        url: url,
+        method: method,
+        headers: {
+          'User-Agent': 'nodeSDK',
+          'x-api-key': apiKey,
+          'x-pii': banishItem
+        }
+      };
+    } else {
+      // Assemble the URL Request
+      var url = config.baseURL+'/'+config.baseVersion+'/'+encodeURI(username)+'/'+encodeURI(banishItem);
+      var options = {
+        url: url,
+        method: method,
+        headers: {
+          'User-Agent': 'nodeSDK',
+          'x-api-key': apiKey
+        }
+      };
+    }
 
     function httpCallback(error, response, body) {
       if (!error && response.statusCode == 200) {
@@ -112,20 +153,34 @@ module.exports = {
   },
 
   // Delete an item from BanishBot, once this is actioned no record of it is kept on BanishBot servers.
-  deleteItemFromBanishBot: function(username, apiKey, banishItem, callback){
+  deleteItemFromBanishBot: function(username, apiKey, banishItem, pii, callback){
     var deferred = Q.defer();
-    // Assemble the URL Request
-    var url = config.baseURL+'/'+config.baseVersion+'/'+encodeURI(username)+'/'+encodeURI(banishItem)
     var method = 'DELETE';
 
-    var options = {
-      url: url,
-      method: method,
-      headers: {
-        'User-Agent': 'nodeSDK',
-        'x-api-key': apiKey
-      }
-    };
+    if(pii == true){
+      // Assemble the URL Request
+      var url = config.baseURL+'/'+config.baseVersion+'/'+encodeURI(username)+'/pii';
+      var options = {
+        url: url,
+        method: method,
+        headers: {
+          'User-Agent': 'nodeSDK',
+          'x-api-key': apiKey,
+          'x-pii': banishItem
+        }
+      };
+    } else {
+      // Assemble the URL Request
+      var url = config.baseURL+'/'+config.baseVersion+'/'+encodeURI(username)+'/'+encodeURI(banishItem);
+      var options = {
+        url: url,
+        method: method,
+        headers: {
+          'User-Agent': 'nodeSDK',
+          'x-api-key': apiKey
+        }
+      };
+    }
 
     function httpCallback(error, response, body) {
       if (!error && response.statusCode == 200) {
